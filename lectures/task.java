b2b1a5771f474df673606b4f02c80ca415496cef
bonew <bonew@bonew.ru>
2023-01-18 21:44:21 +0100

    hello world!

--- lectures/task.java
+++ lectures/task.java
@@ -1,9 +1,8 @@
-package Лекции;
-
+package lectures;
 
 public class task {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("Hello World!");
     }
 }
