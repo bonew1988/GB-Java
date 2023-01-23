a3be8967b24cdb3c3ad30c1bd07a241b38a468a7
bonew <bonew@bonew.ru>
2023-01-23 10:53:04 +0300

    add hw1

---
+++ HW/HW1/task001.java
@@ -0,0 +1,31 @@
+package HW.HW1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class task001 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int n = 0;
+            boolean inputError = true;
+            while (inputError ) {
+                try {
+                    System.out.print("Введите число n: ");
+                    n = input.nextInt();
+                    inputError = false;
+                } catch (InputMismatchException e) {
+                    System.out.println("Некорректный ввод!");
+                    input.next();                
+                }
+                
+            }
+            int result = triangularNumber(n);
+            System.out.println("Треугольное число " + n + " равно: " + result);
+        }
+    }
+    public static int triangularNumber(int x) {
+        return (x* (x + 1))/2;
+        
+    }
+}
