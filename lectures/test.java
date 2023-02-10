940c1a1e438bce01c495ece337c1c0c4de661a8c
bonew <bonew@bonew.ru>
2023-02-10 21:04:27 +0300

    add OOP folder

---
+++ lectures/test.java
@@ -0,0 +1,10 @@
+package lectures;
+
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Current date and time: " + date);
+    }
+}
